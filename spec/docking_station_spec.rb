d31011215d455c4cc1c095ecb6d64035db85c69b
Kaari Strack <kaari.strack@gmail.com>
2018-01-23 15:26:11 +0000

    added method to release_bike from DockingStation class

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,4 +1,6 @@
 require "docking_station"
 describe DockingStation do
-
+  it "responds to the method release_bike" do
+    expect(subject).to respond_to :release_bike # respond_to !!!!!!!!
+  end
 end
