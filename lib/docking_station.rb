58abf7d6cbd07feb2775b89d133d2ff3dbffb9f6
Kaari Strack <kaari.strack@gmail.com>
2018-01-25 09:23:30 +0000

    added attr_reader, and enabled bike docking

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,12 @@
 require_relative "bike"
 
 class DockingStation
+  attr_reader :bike
   def release_bike
     Bike.new # creates a new instance of "Bike"
+  end
+
+  def dock(bike)
+    @bike = bike
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,10 +1,26 @@
 require 'docking_station'
 
 describe DockingStation do
+
   it { is_expected.to respond_to :release_bike }
+
+  it { is_expected.to respond_to(:dock).with(1).argument }
+
+  it { is_expected.to respond_to(:bike) }
+
+  it "docks something" do
+    bike = Bike.new # new instance
+    expect(subject.dock(bike)).to eq bike # we want to return the bikes we dock
+  end
 
   it "releases working bikes" do
     bike = subject.release_bike # calling a class in spec, subject = self!
     expect(bike).to be_working # plain english
+  end
+
+  it "returns docked bikes" do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike). to eq bike
   end
 end
