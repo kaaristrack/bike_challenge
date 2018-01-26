fea0109b6a6714f06e0bba49de905c8a381cb605
Kaari Strack <kaari.strack@gmail.com>
2018-01-26 12:44:04 +0000

    updated style

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -8,29 +8,22 @@ class DockingStation
   end
 
   def release_bike
-     empty?
+    raise 'Docking station empty!' if empty?
     @bicycles.pop
   end
 
   def dock(bike)
-    full?
+    raise 'Docking station full!' if full?
     @bicycles << bike
   end
 
   private
-    def empty?
-      if @bicycles.count.empty?
-        raise StandardError.new("Docking station empty!")
-      else
-        false
-      end
-    end
 
-    def full?
-      if @bicycles.count >= DEFAULT_CAPACITY
-        raise StandardError.new("Docking station full!")
-      else
-        false
-      end
-    end
+  def empty?
+    @bicycles.empty?
+  end
+
+  def full?
+    @bicycles.count >= DEFAULT_CAPACITY
+  end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -8,15 +8,20 @@ describe DockingStation do
 
   it { is_expected.to respond_to(:bicycles) }
 
+  it { is_expected.not_to respond_to(:full?) }
+  it { is_expected.not_to respond_to(:empty?) }
+  # This checks that the private methods are private
+
   describe '#dock' do
 
-    it "raises an error when dock is full" do
-      20.times{subject.dock Bike.new}
-      expect {subject.dock Bike.new}.to raise_error(StandardError)
+    it 'raises an error when dock is full' do
+      described_class::DEFAULT_CAPACITY.times { subject.dock Bike.new }
+      # DockingStation::DEFAULT_CAPACITY searches from the current file position
+      expect { subject.dock Bike.new }.to raise_error(StandardError)
     end
 
-    it "raises an error when dock is empty" do
-      expect{subject.release_bike}.to raise_error(StandardError)
+    it 'raises an error when dock is empty' do
+      expect { subject.release_bike }.to raise_error(StandardError)
     end
   end
 end
